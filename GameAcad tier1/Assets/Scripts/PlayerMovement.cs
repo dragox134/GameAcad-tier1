860ef045b73e32a9b7a4dd36ef69bec3a59f14ba
dragox134 <legi1810@gmail.com>
2022-09-17 21:34:41 +0200

    Ladder

--- GameAcad tier1/Assets/Scripts/PlayerMovement.cs
+++ GameAcad tier1/Assets/Scripts/PlayerMovement.cs
@@ -17,29 +17,57 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    public bool ladderControls = false;
+    public float ladderSpeed = 0.1f;
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(grondCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (ladderControls == false)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
         }
+        else
+        {
+            Vector3 LadderMove = transform.right * x + transform.up * z;
 
-        velocity.y += gravity * Time.deltaTime;
+            controller.Move(LadderMove * ladderSpeed * Time.deltaTime);
+        }
+    }
 
-        controller.Move(velocity * Time.deltaTime);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            ladderControls = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ladder"))
+        {
+            ladderControls = false;
+        }
     }
 }
