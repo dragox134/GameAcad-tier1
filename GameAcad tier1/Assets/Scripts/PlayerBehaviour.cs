d3cb2ad16d7abc2a913275431b2ab05b651ffddf
dragox134 <legi1810@gmail.com>
2022-09-18 15:20:17 +0200

    textmeshpro zaciatok health baru

---
+++ GameAcad tier1/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float health;
+    public float maxHealth = 100;
+    public float deltaHealthPercentage;
+
+    public TextMeshPro healthText;
+    public TextMeshPro dsadew;
+
+    public Transform healthBar;
+
+    public float fallSpeed = .1f;
+    void Start()
+    {
+        health = maxHealth;
+        deltaHealthPercentage = health / maxHealth;
+    }
+    void FixedUpdate()
+    {
+        float healthPercentage = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        healthText.text = (Mathf.RoundToInt(healthPercentage * 100)).ToString() + "%";
+
+        deltaHealthPercentage = Mathf.Lerp(deltaHealthPercentage, healthPercentage, fallSpeed);
+
+        healthBar.localScale = new Vector3(deltaHealthPercentage, 1, 0);
+    }
+}
