c749674334cdc510b1bde843ec6daa2de9241bb3
dragox134 <legi1810@gmail.com>
2022-09-29 21:26:45 +0200

    optionsMenu in menu finnished

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,11 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public void SetVolume(float volume)
     {
@@ -15,5 +54,10 @@ public class SettingsMenu : MonoBehaviour
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void Fullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
